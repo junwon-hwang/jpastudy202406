ad6d8369c40b6f3d54b5dfdb40548fc4b277f831
junwon-hwnag <nyong1320@gmail.com>
2024-06-25 12:45:45 +0900

    # 4. JPA Query method, native SQL

---
+++ src/main/java/com/spring/jpastudy/chap02/entity/Student.java
@@ -0,0 +1,36 @@
+package com.spring.jpastudy.chap02.entity;
+
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
+@Table(name="tbl_student")
+public class Student {
+
+
+    @Id
+    @Column(name = "stu_id")
+    // 랜덤문자로 PK 지정
+    @GeneratedValue(generator = "uid")
+    @GenericGenerator(strategy = "uuid",name="uid")
+    private String id;
+
+    @Column(name = "stu_name" , nullable = false)
+    private String name;
+
+    private String city;
+    private String major;
+
+
+}
