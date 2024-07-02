788b281a1a5ad46bbaf864af2971a1830aaf22c9
junwon-hwnag <nyong1320@gmail.com>
2024-07-02 10:33:19 +0900

    # 27~28. event api 생성 및 querydsl 적용

---
+++ src/main/java/com/spring/jpastudy/event/repository/EventRepositoryCustom.java
@@ -0,0 +1,17 @@
+package com.spring.jpastudy.event.repository;
+
+import com.spring.jpastudy.event.entity.Event;
+
+import java.util.List;
+
+public interface EventRepositoryCustom {
+
+    List<Event> findEvents(String sort);
+
+    //...
+
+    //...
+
+    //...
+
+}
